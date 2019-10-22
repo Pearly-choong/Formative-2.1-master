c909fbac3e71d6f174daa7cf86d35937d386c7b6
[Pearly-choong] <[choongpl@gmail.com]>
2019-10-23 10:10:08 +1300

    completed

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ function findMax(aNums) {
             iMax = aNums[iCount];
         }
     }
-    console.log(iMax);
-    console.log(aNums);
+
+    console.log(iMax, aNums);
     return iMax;
+
 }
 
 
@@ -22,19 +23,22 @@ var iSize = parseInt(prompt('Class size?'));
 var aGradesPhysics = [];
 var aGradesChemistry = [];
 var aGradesBiology = [];
-var iTopGrade = 0;
+
 
 //ParseInt does the same as Number but removes anything other than a number from the value if input
 //Example parseInt(abc123) will give you 123
 for(var iCount=0; iCount<iSize; iCount++) {
     var iGradePhysics = parseInt(prompt('Student '+(iCount+1)+': Grade for Physics out of 100'));
     aGradesPhysics.push(iGradePhysics);
+    console.log(aGradesPhysics);
 
     var iGradeChemistry = parseInt(prompt('Student '+(iCount+1)+': Grade for Biology out of 100'));
     aGradesChemistry.push(iGradeChemistry);
+    console.log(aGradesPhysics);
 
     var iGradeBiology = parseInt(prompt('Student '+(iCount+1)+': Grade for Chemistry out of 100'));
     aGradesBiology.push(iGradeBiology);
+    console.log(aGradesPhysics);
 }
 
 // processing: calculate average grade for each student and find top grade in class
@@ -42,18 +46,19 @@ var aAverageGrades = [];
 
 
 for(var iCount=0; iCount<iSize; iCount++) {
-    var fAverage = (aGradesPhysics[iCount] + aGradesChemistry[iCount] + aGradesBiology[iCount]) / aGradesPhysics.length;
+    var fAverage = ((aGradesPhysics[iCount] + aGradesChemistry[iCount] + aGradesBiology[iCount]) / 3).toFixed();
     aAverageGrades.push(fAverage);
+    console.log(aAverageGrades);
 }
 
 var iTopGrade = findMax(aAverageGrades);
-
+console.log(iTopGrade);
 
 //output: list result for each student and top grade
 
 
-for(var iCount=1; iCount<aAverageGrades.length; iCount++) {
-    if(aAverageGrades[iCount] < 50) {
+for(var iCount=0; iCount<aAverageGrades.length; iCount++) {
+    if(aAverageGrades[iCount] <= 50) {
 
         document.write('Student ' + (iCount+1) + ': Fail </br>');
 
@@ -66,5 +71,6 @@ for(var iCount=1; iCount<aAverageGrades.length; iCount++) {
         document.write('Student ' + (iCount+1) + ': Pass with Distinction </br>');
     }
 }
+
 
 document.write('<h1>The top overall grade is '+ iTopGrade + '</h1>');
